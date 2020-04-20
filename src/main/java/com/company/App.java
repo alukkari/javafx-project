6d9b4c501548f1979eac64bb5ac43bbfe35f9501
Ari Lukkari <ari.lukkari@gmail.com>
2020-04-20 10:50:15 +0300

    changed window title again

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,14 +1,56 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.*;
+
 
 public class App extends Application {
+
+    private Button clearBtn;
+    public App() {
+        System.out.println("constructor");
+    }
+
+    @Override
+    public void init() {
+        System.out.println("init");
+    }
+
     @Override
     public void start(Stage stage) {
-        stage.setTitle("ALukkari");
+
+        clearBtn = new Button("CLEAR");
+
+        stage.setTitle("JavaFX Code Editor");
+        BorderPane borderP = new BorderPane();
+        borderP.setTop(clearBtn);
+        borderP.setCenter(new TextArea());
+
+        Scene scene = new Scene(borderP, 640, 480);
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setScene(scene);
+        //stage.setWidth(640);
+        //stage.setHeight(480);
+        stage.centerOnScreen();
         stage.show();
+
+        //Scene scene = new Scene(new Button("Click!"));
+
+        //Group group = new Group(new Button("Hello"), new Button("World"));
+        //Scene scene = new Scene(group, 640, 480);
+        //stage.setScene(scene);
     }
+
+    @Override
+    public void stop() {
+        System.out.println("stop");
+    }
+
     public static void main(String args[]) {
         launch(args);
     }
