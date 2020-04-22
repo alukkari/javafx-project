1fe67faed4a40e1f3d40bb9eab91f4630b8ed4fb
Ari Lukkari <ari.lukkari@gmail.com>
2020-04-22 09:49:48 +0300

    font, size, color

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,25 +1,38 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.*;
 
 import java.io.File;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 
 public class App extends Application {
 
     private Button clearBtn;
+    private ComboBox fontCBox;
+    private TextField fontSize;
+    private ColorPicker colorPicker;
     private TextArea textAlue;
 
     public App() {
@@ -57,8 +70,32 @@ public class App extends Application {
         textAlue.paste();
     }
 
+    public String hexToRgb(Color color) {
+        final int MAX = 255;
+        return "rgb("
+                + (color.getRed() * MAX)
+                + "," + (color.getGreen() * MAX)
+                + "," + (color.getBlue() * MAX)
+                + ")";
+    }
+
+    public void styleChange(){
+        String textFont = fontCBox.getValue().toString();
+        int size = Integer.parseInt(fontSize.getText());
+        Color value = colorPicker.getValue();
+        String textColor = hexToRgb(value);
+        textAlue.setStyle("-fx-font-family: " + textFont + ";" + "-fx-font-size: " + size + ";" + "-fx-text-fill: " + textColor + ";" );
+        textAlue.requestFocus();
+        textAlue.positionCaret(textAlue.getText().length());
+    }
+
     @Override
     public void start(Stage stage) {
+
+        //Locale locale = Locale.getDefault();
+        Locale locale = new Locale("fi", "FI");
+        ResourceBundle labels = ResourceBundle.getBundle("ui", locale);
+        String title = labels.getString("title");
 
         MenuBar menubar = new MenuBar();
         FileChooser fileChooser = new FileChooser();
@@ -110,25 +147,55 @@ public class App extends Application {
         exit.setOnAction(this::exitValittu);
         about.setOnAction(this::aboutValittu);
 
-        clearBtn = new Button("CLEAR");
         textAlue = new TextArea();
 
+        clearBtn = new Button("CLEAR");
         clearBtn.setOnAction(actionEvent -> textAlue.setText(""));
 
+        //FONT
+        fontCBox = new ComboBox();
+        List<String> fontFamilies = Font.getFamilies();
+        fontCBox.setItems(FXCollections.observableArrayList(fontFamilies));
+        fontCBox.setOnAction(e -> styleChange());
+
+        //FONT SIZE
+        fontSize = new TextField();
+        fontSize.setPrefColumnCount(3);
+        fontSize.setOnAction(e -> styleChange());
+
+        //COLORPICKER
+        colorPicker = new ColorPicker();
+        colorPicker.setOnAction(e -> styleChange());
+
+        //DEFAULT VALUES AT START
+        fontCBox.setValue("Arial");
+        fontSize.setText("12");
+        colorPicker.setValue(Color.valueOf("Black"));
+
         ToolBar toolbar = new ToolBar();
-        toolbar.getItems().addAll(clearBtn);
+        toolbar.getItems().addAll(clearBtn, new Separator(), fontCBox, new Separator(), fontSize, new Separator(), colorPicker);
         VBox ylapalkki = new VBox(menubar, toolbar);
 
-        stage.setTitle("JavaFX Code Editor");
+        stage.setTitle(title);
         BorderPane borderP = new BorderPane();
         borderP.setTop(ylapalkki);
         borderP.setCenter(textAlue);
+
+        textAlue.setOnKeyPressed(keyEvent -> {
+            if(keyEvent.getCode() == KeyCode.TAB) {
+                //System.out.println("tab painettu");
+                int cursorIndex = textAlue.getCaretPosition();
+                textAlue.replaceText(cursorIndex-1, cursorIndex, "    ");
+            }
+        });
 
         Scene scene = new Scene(borderP, 640, 480);
         stage.initStyle(StageStyle.DECORATED);
         stage.setScene(scene);
         stage.centerOnScreen();
         stage.show();
+        textAlue.requestFocus();
+
     }
 
     @Override
